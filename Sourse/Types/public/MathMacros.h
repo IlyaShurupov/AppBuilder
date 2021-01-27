c1c12b6175417498c6b655e75f25a9867348d830
Htto-Ya <63184036+Htto-Ya@users.noreply.github.com>
2021-01-27 19:49:48 +0300

    sdfsd

--- Sourse/Types/public/MathMacros.h
+++ Sourse/Types/public/MathMacros.h
@@ -21,6 +21,9 @@ typedef int SCR_UINT;
 
 #define _CRTDBG_MAP_ALLOC
 #define NAME(v) #v
+
+
+
 #ifdef _DEBUG
 #define DBG_NEW new (_NORMAL_BLOCK, __FILE__, __LINE__)
 // Replace _NORMAL_BLOCK with _CLIENT_BLOCK if you want the
--- Sourse/Types/public/Types.h
+++ Sourse/Types/public/Types.h
@@ -1,13 +1,8 @@
 #pragma once
 
-#include "MathMacros.h"
-#include "Property.h"
-#include "Vec3.h"
-#include "Matrix.h"
 #include "Ray.h"
 #include "Stack.h"
 #include "LinkedList.h"
 #include "FrameBuff.h"
-#include "Object.h"
 #include "Rect.h"
 #include "Timer.h"
